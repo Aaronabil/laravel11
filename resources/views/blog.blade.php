b638a174b9a26e45a7e6132470091f04b9e90c20
Aaronabil <antomi.tomi1979@gmail.com>
2024-10-20 21:03:42 +0700

    view data

--- resources/views/blog.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-  <x-slot:title>{{$title}}</x-slot>
-  <h3 class="text-xl">Kicau</h3>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,8 +11,48 @@ Route::get('/about', function () {
     return view('about', ['nama' => 'Muhamad Nabil'], ['title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts'=> [
+        [
+            'id' => '1',
+            'slug' => 'Judul-1',
+            'title' => 'Judul 1',
+            'author' => 'Muhamad Nabil',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae nobis sit, laboriosam libero incidunt inventore aliquid sapiente. Provident minus fuga assumenda. Nulla non repellat aliquid, laboriosam qui corrupti exercitationem aut.'
+        ],
+        [
+            'id' => '2',
+            'slug' => 'Judul-2',
+            'title' => 'Judul 2',
+            'author' => 'Muhamad Nabil',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi, iusto. Totam odit dicta quaerat iste nulla, officiis ea voluptatum dolores at iure temporibus maiores unde, eos pariatur? Laudantium, pariatur facere.'
+        ]
+    ]]);
+});
+
+Route::get('/posts/{slug}', function ($slug){
+    $posts = [
+        [
+            'id' => '1',
+            'slug' => 'Judul-1',
+            'title' => 'Judul 1',
+            'author' => 'Muhamad Nabil',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae nobis sit, laboriosam libero incidunt inventore aliquid sapiente. Provident minus fuga assumenda. Nulla non repellat aliquid, laboriosam qui corrupti exercitationem aut.'
+        ],
+        [
+            'id' => '2',
+            'slug' => 'Judul-2',
+            'title' => 'Judul 2',
+            'author' => 'Muhamad Nabil',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi, iusto. Totam odit dicta quaerat iste nulla, officiis ea voluptatum dolores at iure temporibus maiores unde, eos pariatur? Laudantium, pariatur facere.'
+        ]
+    ];
+
+    $post = Arr::first($posts, function($post) use($slug){
+        return $post['slug'] == $slug; 
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
