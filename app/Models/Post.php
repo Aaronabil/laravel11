bec2da72bf60d6614764b6e6381137f9356674da
Aaronabil <antomi.tomi1979@gmail.com>
2024-10-22 20:23:40 +0700

    eloquent and post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,39 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Post{
-    public static function all(){
-        return[
-            [
-                'id' => '1',
-                'slug' => 'Judul-1',
-                'title' => 'Judul 1',
-                'author' => 'Muhamad Nabil',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae nobis sit, laboriosam libero incidunt inventore aliquid sapiente. Provident minus fuga assumenda. Nulla non repellat aliquid, laboriosam qui corrupti exercitationem aut.'
-            ],
-            [
-                'id' => '2',
-                'slug' => 'Judul-2',
-                'title' => 'Judul 2',
-                'author' => 'Muhamad Nabil',
-                'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi, iusto. Totam odit dicta quaerat iste nulla, officiis ea voluptatum dolores at iure temporibus maiores unde, eos pariatur? Laudantium, pariatur facere.'
-            ]    
-        ];
-    }
-
-    public static function find($slug):array{
-        // return Arr::first(static::all(), function($post) use($slug){
-        //     return $post['slug'] == $slug; 
-        // });
-
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-
-        if (!$post){
-            abort(404);
-        }
-
-        return $post;
-    }
+class Post extends Model
+{ 
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
